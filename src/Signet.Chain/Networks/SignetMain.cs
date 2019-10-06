8fd6fd9b864f3d142c6a9554fc1b5105a5bc6cdc
tcorvin <tyson.corvin@gmail.com>
2019-10-06 15:27:21 -0400

    header version rule

--- src/Signet.Chain/Networks/SignetMain.cs
+++ src/Signet.Chain/Networks/SignetMain.cs
@@ -76,14 +76,18 @@ namespace Signet.Networks
 
             this.Genesis = genesisBlock;
 
-            // Taken from StratisX.
-            var consensusOptions = new PosConsensusOptions(
+            // Taken from StratisX.    // $!!$ tac
+            var consensusOptions = new SignetPosConsensusOptions(
                 maxBlockBaseSize: 1_000_000,
                 maxStandardVersion: 2,
                 maxStandardTxWeight: 100_000,
                 maxBlockSigopsCost: 20_000,
                 maxStandardTxSigopsCost: 20_000 / 5
             );
+
+         //$!!$
+         consensusOptions.EnforceMinProtocolVersionAtBlockHeight = 50000; // setting the value to zero makes the functionality inactive
+
 
             var buriedDeployments = new BuriedDeploymentsArray
             {
@@ -216,7 +220,7 @@ namespace Signet.Networks
                 //.Register<StratisBugFixPosFutureDriftRule>()
                 .Register<PosFutureDriftRule>()
                 .Register<CheckDifficultyPosRule>()
-                //.Register<StratisHeaderVersionRule>()
+                .Register<StratisHeaderVersionRule>() //$!!$ tac
                 .Register<ProvenHeaderSizeRule>()
                 .Register<ProvenHeaderCoinstakeRule>();
 
